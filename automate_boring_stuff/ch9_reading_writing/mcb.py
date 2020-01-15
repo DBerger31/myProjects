e838809c3df7c503c3f9e794c1550c59a9a10c8b
Dberger31 <DanielBergerNY@gmail.com>
2020-01-15 18:39:45 -0500

    initial modification

--- automate_boring_stuff/ch9_reading_writing/mcb.py
+++ automate_boring_stuff/ch9_reading_writing/mcb.py
@@ -1,2 +1,14 @@
-print("hello world")
-print("I\'ve done this through the terminal")
+# Updatable Multi-clipboard project
+# mcb.py - Saves and loads pieces of text to the clipboard
+# Usage:
+# py.exe mcb.py save <keyword> - Saves clipboard to keyword
+# py.exe mcb.py <keyword> - Loads keyword to clipboard
+# py.exe mcb.py list - Loads all keywords to clipboard
+
+import shelve, pyperclip, sys
+
+mcbShelf = shelve.open('mcb')
+
+#TODO: Save clipboard content
+
+#TODO: List keywords and load content
