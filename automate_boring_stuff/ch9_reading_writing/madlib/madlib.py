70f3d525e84c2450de8513e987a7b50d387b2c28
Dberger31 <DanielBergerNY@gmail.com>
2020-01-19 20:44:02 -0500

    mad libs

--- automate_boring_stuff/ch9_reading_writing/madlib/madlib.py
+++ automate_boring_stuff/ch9_reading_writing/madlib/madlib.py
@@ -13,4 +13,33 @@ madlibs = open(filename)
 text = madlibs.read()
 madlibs.close()
 
-#TODO: print the text and look for what to replace (prompt user to pick words for replacement)
+adjective = re.compile(r'ADJECTIVE*')
+noun = re.compile(r'NOUN*')
+adverb = re.compile(r'ADVERB*')
+verb = re.compile(r'VERB*')
+
+#TODO: Fix issue with not being able to replace additional
+# occurences of adjectives ,verbs, etc.
+
+for word in text.split():
+    if adjective.search(word):
+        madword = input("Enter an ADJECTIVE: ")
+        text = re.sub(r"ADJECTIVE", madword, text, count = 1)
+    elif noun.search(word):
+        madword = input("Enter an NOUN: ")
+        text = re.sub(r"NOUN", madword, text, count = 1)
+    elif adverb.search(word):
+        madword = input("Enter an ADVERB: ")
+        text = re.sub(r"ADVERB", madword, text, count = 1)
+    elif verb.search(word):
+        madword = input("Enter an VERB: ")
+        text = re.sub(r"VERB", madword, text, count = 1)
+
+print('\n' + text)
+
+
+filename = "new" + filename
+newfile = open(filename, "w+")
+newfile.write(text)
+newfile.close()
+
