247bbac161230dd0f658400fe17bb4ad2009ea83
Dberger31 <DanielBergerNY@gmail.com>
2020-02-07 17:52:25 -0500

    fixed missing downloads with try and except

--- automate_boring_stuff/ch12_webscraping/downloadXkcd.py
+++ automate_boring_stuff/ch12_webscraping/downloadXkcd.py
@@ -17,16 +17,23 @@ while not url.endswith('#'):
     ComicElem = soup.select('#comic > img')
     if ComicElem == []: #if there is no comic
         print("Could not find comic image.")
-    else:#middleContainer > ul:nth-child(4) > li:nth-child(2) > a
-        ComicUrl = 'https:' + ComicElem[0].get('src')
-        print('Downloading page %s...' % ComicUrl)
-        res = requests.get(ComicUrl)
-        res.raise_for_status()
+    else:
+        try:
+            ComicUrl = 'https:' + ComicElem[0].get('src')
+            print('Downloading page %s...' % ComicUrl)
+            res = requests.get(ComicUrl)
+            res.raise_for_status()
 
 # open/creates a a file called the last part of comic url (os.path.basename returns just the last part) in xkcd folder
-        imageFile = open(os.path.join('xkcd', os.path.basename(ComicUrl)), 'wb') # write binary to save it
-        for chunk in res.iter_content(100000):
-            imageFile.write(chunk)
+        except requests.exceptions.MissingSchema:
+            #skip the comic
+            prevLink = soup.select('a[rel="prev"]')[0]
+            url = 'https://xkcd.com' + prevLink.get('href')
+            continue # returns to the begging of the while loop
+
+    imageFile = open(os.path.join('xkcd', os.path.basename(ComicUrl)), 'wb')  # write binary to save it
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
 
     prevLink = soup.select('a[rel="prev"]')[0]
     url = 'https://xkcd.com' + prevLink.get('href')
