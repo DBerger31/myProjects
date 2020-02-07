e8465252538939172504de4ca3442842827d19f8
Dberger31 <DanielBergerNY@gmail.com>
2020-02-07 17:33:50 -0500

    downloads images but fails on image 2067

--- automate_boring_stuff/ch12_webscraping/downloadXkcd.py
+++ automate_boring_stuff/ch12_webscraping/downloadXkcd.py
@@ -7,19 +7,29 @@ import requests, os, bs4
 url = 'https://xkcd.com/' # Staring URL
 os.makedirs('xkcd', exist_ok = True) #store comics in ./xkcd
 
-while not url.endswith('#')
+while not url.endswith('#'):
     #Downloads page
     print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
-
     soup = bs4.BeautifulSoup(res.text, 'html.parser') # HTML CODE in soup var
 
-#TODO: FIND THE URL OF THE COMIC IMAGE
-linkElems = soup.select('#bottom > a')
-print(linkElems)
+    ComicElem = soup.select('#comic > img')
+    if ComicElem == []: #if there is no comic
+        print("Could not find comic image.")
+    else:#middleContainer > ul:nth-child(4) > li:nth-child(2) > a
+        ComicUrl = 'https:' + ComicElem[0].get('src')
+        print('Downloading page %s...' % ComicUrl)
+        res = requests.get(ComicUrl)
+        res.raise_for_status()
 
-#TODO: DOWNLOAD THE IMAGE
+# open/creates a a file called the last part of comic url (os.path.basename returns just the last part) in xkcd folder
+        imageFile = open(os.path.join('xkcd', os.path.basename(ComicUrl)), 'wb') # write binary to save it
+        for chunk in res.iter_content(100000):
+            imageFile.write(chunk)
+
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = 'https://xkcd.com' + prevLink.get('href')
 
 #TODO: SAVE THE IMAGE TO ./xkcd
 
