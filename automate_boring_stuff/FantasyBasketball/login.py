08dd92f5fc7b5787a01ad37ccbc4f85c674900f2
Daniel Berger <DanielBergerNY@gmail.com>
2022-09-16 20:28:40 -0400

    update

--- automate_boring_stuff/FantasyBasketball/login.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(r"C:\Users\Danie\Desktop\githubProjects\myProjects\FantasyBasketball\driver\chromedriver.exe")
-
-driver.set_page_load_timeout(10)
-
